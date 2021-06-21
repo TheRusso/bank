e5ce8cdf60a7bb1f37ace1446289ca44106f1125
Ruslan Humenyuk <ruslan21343@gmail.com>
2021-06-21 11:53:03 +0300

    Doc

--- src/main/java/com/example/bank/services/TransactionsService.java
+++ src/main/java/com/example/bank/services/TransactionsService.java
@@ -5,6 +5,17 @@ import com.example.bank.model.entities.Transaction;
 import java.util.List;
 
 public interface TransactionsService {
+    /**
+     * Returns List of input Transactions by given card
+     * @param card
+     * @return List of Transactions
+     */
     List<Transaction> findByCardTo(String card);
+
+    /**
+     * Returns List of output Transactions by given card
+     * @param card
+     * @return List of Transactions
+     */
     List<Transaction> findByCardFrom(String card);
 }
--- src/main/java/com/example/bank/services/UserService.java
+++ src/main/java/com/example/bank/services/UserService.java
@@ -6,10 +6,49 @@ import com.example.bank.model.rest_response.AllTransactionsResponse;
 import java.util.Optional;
 
 public interface UserService {
+    /**
+     * Returns Optional<User> by card
+     * @param card
+     * @return Optional<User>
+     *              User entity
+     */
     Optional<User> findByCard(String card);
+
+    /**
+     * Returns Optional<User> by id
+     * @param id
+     * @return Optional<User>
+     *              User entity
+     */
     Optional<User> findById(long id);
+
+    /**
+     * Saves or updates user entity
+     * @param user
+     *          User entity
+     */
     void save(User user);
+
+    /**
+     * Returns AllTransactionsResponse
+     * @param card
+     * @return AllTransactionsResponse
+     *          using for showing transactions in REST
+     */
     AllTransactionsResponse getAllTransactionsResponseByCard(String card);
+
+    /**
+     * Top up money in the account using cash
+     * @param card
+     * @param value
+     */
     void topUp(String card, long value);
+
+    /**
+     * Make transaction between two cards
+     * @param cardFrom
+     * @param cardTo
+     * @param value
+     */
     void transact(String cardFrom, String cardTo, long value);
 }
